7796ae1e8002155e9f4850fa57d415967bde0ca7
JoshPrpic <JoshPrpic@gmail.com>
2015-12-10 13:54:14 -0500

    Master version 1.1

--- Wukong-Adventure/src/testClass/Board.java
+++ Wukong-Adventure/src/testClass/Board.java
@@ -3,33 +3,41 @@ package testClass;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.event.*;
-
+import java.awt.event.*;	
 import javax.swing.*;
 
 //a kind of panel that has all objects (like Wukong) on it
 public class Board extends JPanel implements ActionListener{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	Character wukong;
 	Image img; //contains background image
 	Timer time; //timer to update the frames(so we SEE something happen)
 	
 	public Board(){
 		wukong = new Character();
-		setFocusable(true); //allows charcter to move on board
-		ImageIcon i = new ImageIcon("testcharacter.png"); // gets image
+		addKeyListener(new AL());
+		setFocusable(true); //allows character to move on board
+		ImageIcon i = new ImageIcon(this.getClass().getResource("sky.png")); // gets image
 		img = i.getImage();
 		time = new Timer(5, this);
 		time.start();
 	}
 	
 	public void actionPerformed(ActionEvent e){
+		wukong.move();
 		repaint();
 	}
 	
 	public void paint(Graphics g){
 		super.paint(g);
 			Graphics2D g2d = (Graphics2D) g;
+			
+			g2d.drawImage(img, 0, 0, null);
+			g2d.drawImage(wukong.getImage(), wukong.getX(), wukong.getY(), null);
 	}
 	
 	//this is the action listener class
@@ -38,11 +46,12 @@ public class Board extends JPanel implements ActionListener{
 		
 		//these methods are self explanatory
 		public void keyReleased(KeyEvent e){
-			
+			wukong.keyReleased(e);
 		}
 		
 		public void keyPressed(KeyEvent e){
-			
+			wukong.keyPressed(e);
 		}
 	}
+
 }
