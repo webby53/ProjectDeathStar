15a78e549e50f0e0c67e55e1122249812b6a9645
webby53 <kishonwinston@hotmail.com>
2015-12-11 14:11:21 -0500

    VERSION ONE BOYS

--- Wukong-Adventure/src/testClass/TryingGraphics.java
+++ Wukong-Adventure/src/testClass/TryingGraphics.java
@@ -3,6 +3,13 @@ package testClass;
 //trying out graphics and animation
 //this class now has the purpose of posting tasks
 //objectives and group communication
+//
+//
+//
+//
+//
+//
+//
 public class TryingGraphics {
 
 }
---
+++ Wukong-Adventure/src/version1/Game.java
@@ -0,0 +1,17 @@
+package version1;
+
+import java.awt.Canvas;
+
+public class Game extends Canvas implements Runnable{
+
+	public static final String TITLE = "Wukongs Advernture Ver 1.0";
+	public static final int WIDTH = 0;
+	public static final int HEIGHT = WIDTH / 4 * 3;
+
+	public void run(){}
+	
+	public static void main(String[] args){
+		
+	}
+	
+}
