5b60ee032bffbe76cda63e9ce4573e98153112ac
JoshPrpic <JoshPrpic@gmail.com>
2015-12-21 14:01:40 -0500

    Fixed the tile collision problem
    Mobs now react smoothly with the tiles and will not catch on any of the
    sides
    Also removed the gaps that formed between the tiles and player and fixed
    the intersections so the models do not overlap

--- Wukong-Adventure/src/entities/Tile.java
+++ Wukong-Adventure/src/entities/Tile.java
@@ -13,10 +13,10 @@ public class Tile extends Entity{
 	public Tile(double x, double y, Sprite sprite) {
 		super(x, y, sprite);
 		tiles.add(this);
-		recTop = new Rectangle((int)x, (int)y - 1, sprite.getWidth() + 2, 1);
-		recBot = new Rectangle((int)x, (int)y + sprite.getHeight(), sprite.getWidth() + 2, 1);
-		recLeft = new Rectangle((int)x - 3, (int)y, 1, sprite.getHeight());
-		recRight = new Rectangle((int)x + sprite.getWidth(), (int)y, 1, sprite.getWidth());
+		recTop = new Rectangle((int)x + 1, (int)y - 1, sprite.getWidth() - 2, 1);
+		recBot = new Rectangle((int)x + 1, (int)y + sprite.getHeight() - 1, sprite.getWidth() - 2, 1);
+		recLeft = new Rectangle((int)x - 2, (int)y, 1, sprite.getHeight() - 2);
+		recRight = new Rectangle((int)x + sprite.getWidth() + 1, (int)y, 1, sprite.getWidth() - 2);
 	}
 
 	@Override
