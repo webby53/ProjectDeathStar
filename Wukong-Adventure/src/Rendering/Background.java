c6ed190f7f130b8cfc48467964721386c1d84ea1
webby53 <kishonwinston@hotmail.com>
2016-01-08 13:12:55 -0500

    UPDATE

--- Wukong-Adventure/src/Rendering/Background.java
+++ Wukong-Adventure/src/Rendering/Background.java
@@ -23,6 +23,22 @@ public class Background {
 		}
 	}
 	
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
 	public void draw(Graphics2D g){
 		g.drawImage(image, x, y, null);
 		if(x < 0){
