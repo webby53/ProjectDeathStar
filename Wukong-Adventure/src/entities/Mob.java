1864c8d86568d0320da668bbcf41644335213a76
webby53 <kishonwinston@hotmail.com>
2016-01-08 19:37:05 -0500

    PATCH 4.92
    Fixed a merging error

--- Wukong-Adventure/src/entities/Mob.java
+++ Wukong-Adventure/src/entities/Mob.java
@@ -10,14 +10,8 @@ public abstract class Mob extends Entity{
 
 	protected double dx, dy;
 	protected boolean collision;
-<<<<<<< HEAD
 	protected double gravity = 0.5;
 	protected double terminalV = 20;
-=======
-	protected double gravity = 0.2;
-	protected double friction = 0.2;
-	protected double terminalV = 7;
->>>>>>> origin/master
 	protected boolean falling;
 
 	public Mob(double x, double y, Sprite sprite) {
