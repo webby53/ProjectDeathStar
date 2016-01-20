4f7014e3c25dfe404e6de082140d2157ef8deb45
Squibles <simooni@hotmail.ca>
2016-01-19 19:55:57 -0500

    javadoc'd background

--- Wukong-Adventure/src/Rendering/Background.java
+++ Wukong-Adventure/src/Rendering/Background.java
@@ -6,7 +6,9 @@ import java.io.File;
 import java.io.IOException;
 import Main.Game;
 import javax.imageio.ImageIO;
-
+/**@author Joshua Prpic, Kishon Webb, Simon Yacoub
+ *
+ */
 public class Background {
 	
 	private BufferedImage image;
@@ -15,6 +17,9 @@ public class Background {
 	private double move;
 	private double cx;
 	
+	/**Background Constructor 
+	 * 
+	 */
 	public Background(String s, double move){
 		this.move = move;
 		try {
@@ -24,30 +29,52 @@ public class Background {
 		}
 	}
 	
+	/**returns the value of x
+	 * 
+	 * @return x
+	 */
 	public double getX() {
 		return x;
 	}
 
+	/**Sets the value of x
+	 * 
+	 * @param x
+	 */
 	public void setX(int x) {
 		this.x = x * move;
 	}
 
+	/**returns the value of y
+	 * 
+	 * @return y
+	 */
 	public double getY() {
 		return y;
 	}
 
+	/**sets the value of y
+	 * 
+	 * @param y
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 	
+	/**sets the value of cx
+	 * 
+	 * @param dx
+	 */
 	public void setCx(double dx){
 		this.cx = dx;
 	}//updates the location of background
-	public void update(){
-		x += cx;
-	}
 	
-	//draws the background
+	
+	
+	/**draws the background
+	 * 
+	 * @param g
+	 */
 	public void draw(Graphics2D g){
 		g.drawImage(image, (int)x, y, null);
 		if(x < 0){
